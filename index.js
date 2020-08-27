96c3f0ed17ccc95dc9b68d83c12e68e6d94be23b
nikhils <nikhils@aspenify.com>
2020-08-27 17:57:46 +0530

    first table component return

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 function compactTable(min, max) {
-    if (typeof (max) !== 'number' && typeof (min) !== 'number') {
-        min = 0; max = 1;
-    }
-    return (Math.random() * (max - min)) + min;
+    return <table>
+        <th>
+            <td>
+                Name
+            </td>
+            <td>
+                Email
+            </td>
+            <td>
+                Mobile
+            </td>
+        </th>
+        <tr>
+            <td>
+                Jone Deo
+            </td>
+            <td>
+                jonedeo@gmail.com
+            </td>
+            <td>
+                +10232213322
+            </td>
+        </tr>
+    </table>
 }
 module.exports = compactTable;
