f020a86fd7dc10e4ca3d602a8e53448507ba97cc
nikhils <nikhils@aspenify.com>
2021-02-23 17:08:01 +0530

    defects

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-function CompactTable(min, max) {
-    return console.log("WIP")
+function CompactTable() {
+    let table = <table>
+        <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+        </tr>
+    </table>
+    return table
 }
 module.exports = CompactTable;
